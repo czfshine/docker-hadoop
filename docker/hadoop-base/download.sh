3c788fb1fb0cf49ef72996a075fddf1931a6f6b0
czfshine <czfshine@outlook.com>
2019-04-16 23:29:06 +0800

    去掉下载过程中的大量输出

--- docker/hadoop-base/download.sh
+++ docker/hadoop-base/download.sh
@@ -1,5 +1,5 @@
 #下载一些东西
 
-wget http://mirrors.tuna.tsinghua.edu.cn/apache/hadoop/common/hadoop-2.7.7/hadoop-2.7.7.tar.gz
-tar -xzvf hadoop-2.7.7.tar.gz
+wget -q http://mirrors.tuna.tsinghua.edu.cn/apache/hadoop/common/hadoop-2.7.7/hadoop-2.7.7.tar.gz
+tar -xzf hadoop-2.7.7.tar.gz
 
